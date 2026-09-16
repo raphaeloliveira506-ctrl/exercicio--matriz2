122170b70a381ff2f28a4c3d4eef44e034d7b009
Bruno <Bruno@DESKTOP-LIF2M8D>
2026-09-16 16:09:53 -0300

    Adicionar arquivos de projeto.

---
+++ exercicio 2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+
+        int[,] matriz = new int[501, 501];
+
+        bool repetido = false;
+
+        for (int i = 1; i <= N; i++)
+        {
+            string[] dados = Console.ReadLine().Split();
+
+            int x = int.Parse(dados[0]);
+            int y = int.Parse(dados[1]);
+
+            if (matriz[x, y] == 1)
+            {
+                repetido = true;
+            }
+
+            matriz[x, y] = 1;
+        }
+
+        if (repetido == true)
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+            Console.WriteLine(0);
+        }
+    }
+}
